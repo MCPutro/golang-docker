a762fd78f879d2ef1ef0d923d4a39fa2f4389364
Mu'ti C Putro <mcpoetro@gmail.com>
2023-05-10 22:46:48 +0700

    rename full_name to fullname

--- model/web/user_create_request.go
+++ model/web/user_create_request.go
@@ -2,6 +2,6 @@ package web
 
 type UserCreateRequest struct {
 	Username string `json:"username"`
-	FullName string `json:"full_name"`
+	FullName string `json:"fullname"`
 	Password string `json:"password,omitempty"`
 }
